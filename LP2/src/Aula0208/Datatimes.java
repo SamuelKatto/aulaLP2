2b39f713640f904aa38284ea29a5f9706e05c3b2
SamuelKatto <53586055+SamuelKatto@users.noreply.github.com>
2019-08-02 22:34:52 -0400

    Add files via upload

--- LP2/src/Aula0208/Datatimes.java
+++ LP2/src/Aula0208/Datatimes.java
@@ -1,8 +1,37 @@
 package Aula0208;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Datatimes {
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) { // ou essa opção throws InterruptedException
+		Instant inicio = Instant.now();
 		
+		
+		
+// essa opção ou		
+		// thread.sleep(milesegundos) control + 1 		
+		try {
+			Thread.sleep(1000); // fazer o processamento esperar
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		Instant fim = Instant.now();
+		
+		Duration duracao = Duration.between(inicio, fim);
+		
+		System.out.println("Duracao em nanos segundos" + duracao.toNanos());
+		System.out.println("Duracao em minutos" + duracao.toMinutes());
+		System.out.println("Duracao em horas" + duracao.toHours());
+		System.out.println("Duracao em milisegundos" + duracao.toMillis());
+		System.out.println("Duracao em dias" + duracao.toDays());
+
 		
 	}
 
